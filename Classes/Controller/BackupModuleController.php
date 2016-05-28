386e8358aa08ae2f2039e1263ca13b006f1391c4
Andreas Hoffmeyer <a.hoffmeyer@gmx.net>
2016-05-28 09:28:07 +0200

    Adding TYPO3 comment on top

--- Classes/Controller/BackupModuleController.php
+++ Classes/Controller/BackupModuleController.php
@@ -2,6 +2,19 @@
 
 namespace AHoffmeyer\RobotstxtEditor\Controller;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use TYPO3\CMS\Core\Error\Exception;
 use TYPO3\CMS\Core\Http\AjaxRequestHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
